a959ec73707dc3f4ce478cb36b31a7c27fa5b527
jhonatan-aj <an.jhonatan15@gmail.com>
2025-12-19 11:10:51 -0500

    correction especialistas

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
